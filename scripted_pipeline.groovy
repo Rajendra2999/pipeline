a1b16b26706d1a5464751d1455c788ba53d582a8
raj <rajendra@gmail.com>
2022-06-04 07:31:31 +0530

    --- Auto Git Commit ---

--- scripted_pipeline.groovy
+++
@@ -1,8 +0,0 @@
-node {  
-    stage('Pull') { 
-        git credentialsId: 'git-cred', url: 'https://github.com/Rajendra2999/pipeline.git'
-    }
-    stage('Test') { 
-        echo "Test Complete"
-    }
-}
